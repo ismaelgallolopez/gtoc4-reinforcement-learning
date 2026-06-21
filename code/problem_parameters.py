eb913b0297f742eae7e35f562a7f793d54e674b4
Ismael Gallo Lopez <ismaellopez.clase@gmail.com>
2026-06-21 11:35:25 +0200

    modified so the dates are directly epochs

--- code/problem_parameters.py
+++ code/problem_parameters.py
@@ -1,14 +1,16 @@
 # this file contains the parameters for the problem
 
 from tudatpy import constants
+from tudatpy.astro import time_representation
 
 spacecraft_dry_mass = 500.0 #kg
 spacecraft_propellant_mass = 1000.0 # kg
 spacecraft_wet_mass = spacecraft_dry_mass + spacecraft_propellant_mass # kg
 scape_velocity_max = 4.0e3 # m/s
-Isp = 3000.0 # s
+Isp_engine = 3000.0 # s
 thrust_max = 0.135 # N
-launch_interval = [2015, 2025] # years
+launch_interval = [time_representation.DateTime(2015, 1, 1).to_epoch(), 
+                   time_representation.DateTime(2025, 1, 1).to_epoch()]
 time_mission_max = 10.0 * constants.JULIAN_YEAR # s
 
 # earth's orbital elements in the J2000 frame at epoch 
@@ -20,6 +22,6 @@ arg_periapsis_earth = 287.61577546182 # deg
 mean_anomaly_earth = 257.606837077535 # deg
 epoch = 54000.0 # MJD2000
 
-# constraints on the rendevouz
+# constraints on the rendevouz/flyby
 accuracy_position = 1000.0e3 # m
-accuracy_velocity = 1.0 # m/s
+accuracy_velocity = 1.0 # m/s (only for rendezvous)
