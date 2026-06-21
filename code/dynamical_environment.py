48c537579594fa1ff1e7101febd0e424287b28f9
Ismael Gallo Lopez <ismaellopez.clase@gmail.com>
2026-06-21 15:59:34 +0200

    included asteroids initial state and integrator settings

--- code/dynamical_environment.py
+++ code/dynamical_environment.py
@@ -13,7 +13,7 @@ from auxiliary_functions import *
 
 ASTEROIDS_FILEPATH = "../data/gtoc4_problem_data.txt"
 n_asteroids = None # set to None to load all asteroids
-n_asteroids = 1 # optional, to limit the number of asteroids when testing
+n_asteroids = 5 # optional, to limit the number of asteroids when testing
 
 # fixed epoch for first iterations, later will work on implementing the variable one (TODO)
 start_epoch = launch_interval[0]
@@ -28,7 +28,7 @@ names     = [f"ast_{a['name']}" for a in asteroids]
 bodies_to_propagate = ['spacecraft'] + names
 # print("Bodies to propagate:", bodies_to_propagate) # debugging
 
-central_bodies = ['Sun']
+central_bodies = ['Sun'] * len(bodies_to_propagate)
 
 
 # setup the thrust model
@@ -57,11 +57,14 @@ acceleration_settings_spacecraft = dict(
     spacecraft=[    propagation_setup.acceleration.thrust_from_all_engines()]
     )
 
+acceleration_settings_asteroids = dict(
+    Sun=[propagation_setup.acceleration.point_mass_gravity()]
+)
+
 # thrust_direction_settings = propagation_setup.thrust.custom_thrust_orientation(
 #     thrust_direction_function
 # )
 
-acceleration_settings = { 'spacecraft': acceleration_settings_spacecraft }
 
 # computing the initial state of the spacecraft
 true_anomaly_earth = element_conversion.mean_to_true_anomaly(eccentricity_earth, mean_anomaly_earth) # convertion needed for the tudat conversion function
@@ -73,6 +76,17 @@ excess_velocity_vector = np.array([scape_velocity_max, 0, 0]) # dummy direction
 delta_state_departure = np.hstack((np.zeros(3), excess_velocity_vector)) # only velocity change, no position change
 spacecraft_initial_state_cartesian = earth_initial_state_cartesian + delta_state_departure
 
+# initial state of the asteroids 
+true_anomalies_asteroids = [element_conversion.mean_to_true_anomaly(ast['e'], ast['M0']) for ast in asteroids]
+asteroids_initial_states_keplerian = [np.array([ast['a'], ast['e'], ast['i'], ast['lan'], ast['omega'], true_anomaly]) for ast, true_anomaly in zip(asteroids, true_anomalies_asteroids)]
+asteroids_initial_states_cartesian = [element_conversion.keplerian_to_cartesian(keplerian_state, sun_gravitational_parameter) for keplerian_state in asteroids_initial_states_keplerian]
+
+# print(np.shape(asteroids_initial_states_cartesian)) # debugging
+# initial state of the system (spacecraft + asteroids)
+system_initial_state = np.hstack((spacecraft_initial_state_cartesian,
+                                 *asteroids_initial_states_cartesian # need to transpose for consistency
+                                ))
+
 # print("Initial state of the Earth (cartesian):", earth_initial_state_cartesian) # debugging
 # print("Initial state of the spacecraft (cartesian):", spacecraft_initial_state_cartesian) # debugging
 
@@ -81,7 +95,29 @@ time_step = 100.0 # s, dummy value for now, will need to be tuned
 # fixed step RK4 integrator used for simplicity, later to be implemented in a more sophisticated method (TODO)
 integrator_settings = propagation_setup.integrator.runge_kutta_fixed_step( 
     time_step = time_step,
-    coefficients_set = propagation_setup.integrator.CoefficientSets.rk_4
+    coefficient_set = propagation_setup.integrator.CoefficientSets.rk_4
 )
 
+# acceleration settings
+acceleration_settings = { 'spacecraft': acceleration_settings_spacecraft, 
+                         **{name: acceleration_settings_asteroids for name in names} }
 
+# print(bodies, central_bodies, bodies_to_propagate) # debugging
+
+acceleration_models = propagation_setup.create_acceleration_models(
+    bodies, acceleration_settings, bodies_to_propagate, central_bodies
+)
+
+# Create propagation settings.
+termination_settings = propagation_setup.propagator.time_termination(end_epoch)
+
+propagator_settings = propagation_setup.propagator.translational(
+    central_bodies,
+    acceleration_models,
+    bodies_to_propagate,
+    system_initial_state,
+    start_epoch,
+    integrator_settings,
+    termination_settings,
+    # output_variables=dependent_variables_to_save,
+)
